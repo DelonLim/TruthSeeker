d73771e14cc104d36e8d6067239fde1afddf656c
brendontkl <btkl123@gmail.com>
2021-03-26 00:16:10 +0800

    Added back the background and music
    Some errors syncing the intro video back to main menu.

---
+++ Assets/Script/TextBG.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBG : MonoBehaviour
+{
+    Image image;
+    void Start()
+    {
+        image = GetComponent<Image>();
+
+        Color c = image.color;
+        c.a = 0.8f;
+        image.color = c;
+    }
+
+    public void Selectedbtn()
+    {
+        image = GetComponent<Image>();
+
+        Color c = image.color;
+        c.a = 1f;
+        image.color = c;
+    }
+    public void Unselectedbtn()
+    {
+        image = GetComponent<Image>();
+
+        Color c = image.color;
+        c.a = 0.8f;
+        image.color = c;
+    }
+}
