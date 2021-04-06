28db0b8409641379fec7386ceacfd12c324e8a82
brendontkl <btkl123@gmail.com>
2021-04-07 02:48:40 +0800

    savedata.php modified for upload of time taken

--- Assets/xampp/htdocs/truthseekers/savedata.php
+++ Assets/xampp/htdocs/truthseekers/savedata.php
@@ -11,6 +11,7 @@
 	
 	$username = $_POST["name"];
 	$newscore = $_POST["score"];
+	$timetaken = $_POST["time"];
 
 	//double checks that there is only one user with this name
 	$namecheckquery = "SELECT username FROM playerscoredb WHERE username='"  . $username . "';";
@@ -20,14 +21,13 @@
 
 	if (mysqli_num_rows($namecheck) > 0) //if user exists in score database already, just update the score 
 	{
-		$updatequery = "UPDATE playerscoredb SET score = " . $newscore . " WHERE username = '" . $username . "';";
+		$updatequery = "UPDATE playerscoredb SET score = '".$newscore."', timetaken = '".$timetaken ."' WHERE username = '" . $username . "' ";
 		mysqli_query($con, $updatequery) or die("7: Save query failed"); //error code #7 - UPDATE query failed
-
 		echo "0";
 		exit();
 	}
 
-	$insertuserquery = "INSERT INTO playerscoredb (username, score) VALUES ('" . $username .  "', '" . $newscore . "');";
+	$insertuserquery = "INSERT INTO playerscoredb (username,score,timetaken) VALUES ('" . $username .  "', '" . $newscore . "', '" . $timetaken. "')";
 	mysqli_query($con, $insertuserquery) or die("4: Insert player query failed");
 	echo "0";
 	exit();
