c77a23e7799af38cd53e729157fa15f0414b1e8d
clim140 <numberlock16@gmail.com>
2021-04-07 09:13:48 +0800

    Finally working

---
+++ Assets/xampp/htdocs/truthseekers/LoadUniqueWorld.php
@@ -0,0 +1,34 @@
+<?php
+
+	$con = mysqli_connect('localhost', 'root', '', 'unityaccess');
+
+	//check if the connection happens
+	if (mysqli_connect_errno()) 
+	{
+		echo "1: Connection failed"; // error code #1 meaning connection failed
+		exit();
+	}
+
+	$SelectWorldNamequery = "SELECT DISTINCT `worldname` FROM `playerscoredb`";
+	$findWorldName = mysqli_query($con, $SelectWorldNamequery) or die("2: Fail to insert"); //error code #2 meaning namecheck query failed
+
+	if(mysqli_num_rows($findWorldName) < 1)
+	{
+		echo "No Such Name in Database";
+		exit();
+	}
+	$allRow = array();
+	
+	while(($existinginfo = mysqli_fetch_assoc($findWorldName)))
+	{
+		$allRow[] = $existinginfo['worldname'];
+	}	
+
+	for($x = 0; $x<count($allRow);$x++)
+	{
+		echo $allRow[$x].",";
+	}
+
+	exit();
+	
+?>
